8d9d63cf650cd95a45ed2eafa595d3a103f1189d
edwinolono <edwin.olono@gmail.com>
2021-06-16 15:01:13 -0500

    Script de inicio de sesión

--- public/login.php
+++ public/login.php
@@ -1,3 +1,22 @@
 <?php 
-    echo "Aquí irá el login";
+     $con = mysqli_connect(
+        'localhost',
+        'miguel',
+        '1234',
+        'e_chiquitzin'
+    );
+
+    if($_POST)
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+    }
+    $result = mysqli_query($con, "SELECT 'password' FROM 'alumno' WHERE 'e-mail' == $email");
+    if($result == $password){
+        session_start();
+        $_SESSION['miSesion'] = "alumno";
+        echo ("Sesión iniciada");
+    }
+    else
+        echo ("Contraseña o correo incorrectos");
 ?>
