35350e13566c68322f5098da3b5951cd1d6be56c
Guillermo Cardona <memocargo@hotmail.com>
2021-06-24 16:41:05 -0500

    Update Clase8.php
    Estética de clases

--- public/Clase8.php
+++ public/Clase8.php
@@ -1,14 +1,23 @@
-<!DOCTYPE html>
+<?php include 'headerClases.php' ?>
 <htm>
+
     <head>
         <title>Clase 8 contemos hasta 15</title>
         <link rel="stylesheet" href="../../css/paginaComienzo/paginaComienzo.css">
         <link rel="stylesheet" href="../../css/menu/menu.css">
     </head>
+
     <body>
-        <div class="nav-bar">
-            <div class="sub-menu"> <a>Inicio</a> <a>Mis tareas</a> <a>Ayuda</a></div>
-            <h1 class="logo"><a href="../menuAlumno.php">e-Chiquitzin</a></h1>
+        <header>
+            <div id="nombreAplicacion"><a href="../menuAlumno.php">e-Chiquitzin</a></div>
+            <div class="anclajes">
+                <a href="./menuAlumno.php">Estadísticas</a>
+                <a href="./menuAlumno.php">Inicio</a>
+                <a href="./paginaAyuda.php">Ayuda</a>
+                <a href="./cerrarSesion.php">Cerrar Sesión</a>
+            </div>
+        </header>
+        <div>
             <h2 style="text-align: center;">Clase 8 Contemos hasta 15</h2>
         </div>
         <div class="container">
@@ -16,14 +25,14 @@
                 <h2 style="text-align: center;">Las fiestas patrias</h2>
                 <div style="background-color: azure; border-radius: 15px; width: 90%; height: auto; position: relative; left: 15px;">
                     <h3 style="text-align: center;">Identifica cuántos objetos hay en la imagen</h3>
-                    <img src="../../images/Clase 8 img 1.png" style="width: 400px; height: 370px; position: relative; left:5px" ><br>
+                    <img src="../../images/Clase 8 img 1.png" style="width: 400px; height: 370px; position: relative; left:5px"><br>
                     <img src="../../images/Clase 8 img 2.png" style=" width: 50px; height: 50px;"><input type="number" min="0" max="20">
                     <img src="../../images/Clase 8 img 3.png" style=" width: 50px; height: 50px;"><input type="number" min="0" max="20">
                     <img src="../../images/Clase 8 img 4.png" style=" width: 50px; height: 50px;"><input type="number" min="0" max="20"><br>
                     <img src="../../images/Clase 8 img 5.png" style=" width: 50px; height: 50px;"><input type="number" min="0" max="20">
                     <img src="../../images/Clase 8 img 6.png" style=" width: 50px; height: 50px;"><input type="number" min="0" max="20">
                     <img src="../../images/Clase 8 img 7.png" style=" width: 50px; height: 50px;"><input type="number" min="0" max="20">
-                   
+
                 </div>
             </div>
             <div class="right-aside" style="background-color: rgb(238, 139, 9); border-radius: 15px; position: relative; left: 200px;">
@@ -31,11 +40,19 @@
                 <div style="background-color: azure; border-radius: 15px; width: 90%; height: auto; position: relative; left: 25px;">
                     <h3 style="text-align: center;">Jueguen en parejas</h3>
                     <ol>
-                        <li><h4 style="text-align: justify;">Mete en tu caja de sorpresas de 10 a 15 fichas</h4></li>
-                        <li><h4 style="text-align: justify;">Tú te duermes, mientras tu compañero toma y esconde algunas fichas</h4></li>
-                        <li><h4 style="text-align: justify;">Luego despiertas y cuentas las fichas que quedaron en la caja</h4></li>
-                        <li><h4 style="text-align: justify;">Di cuántas fichas tomó tu compañero</h4></li>
-                        
+                        <li>
+                            <h4 style="text-align: justify;">Mete en tu caja de sorpresas de 10 a 15 fichas</h4>
+                        </li>
+                        <li>
+                            <h4 style="text-align: justify;">Tú te duermes, mientras tu compañero toma y esconde algunas fichas</h4>
+                        </li>
+                        <li>
+                            <h4 style="text-align: justify;">Luego despiertas y cuentas las fichas que quedaron en la caja</h4>
+                        </li>
+                        <li>
+                            <h4 style="text-align: justify;">Di cuántas fichas tomó tu compañero</h4>
+                        </li>
+
                         <img src="../../images/Clase 8 img 8.png" style="width: 400px; height: 150px; position: relative; left: 100px;">
                         <h4>¿Cuántas fichas tomó tu compañero? D:</h4>
                         <input type="number" min="0" max="15">
