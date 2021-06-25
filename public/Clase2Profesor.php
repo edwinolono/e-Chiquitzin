0ec09d03b73643812a73cbe820f8cb992b1f1266
Diego Parrila <CgoMacDroid@gmail.com>
2021-06-24 19:12:54 -0500

    Modificacion de la vista clasesProfesor

---
+++ public/Clase2Profesor.php
@@ -0,0 +1,50 @@
+<?php include 'headerClases.php'; ?>
+    <body>
+        
+        <div class="container">
+            <h1>Clase 2 Profesor</h1>
+
+            <form>
+
+                <div class="ejercicio" style="background-color: #F39C12">
+                    <h2>¿Tienen la misma cantidad?</h2>
+                    <h3>Trabajen en Equipo</h3>
+                    <div>
+                        <img src="/images/clases/clase2/Clase2 img 1.png" style="border: 2px solid black; border-radius: 15px; max-width: 90%; left: 15px;">                    
+                        <img src="/images/clases/clase2/Clase2 img 2.png" style="border: 2px solid black; border-radius: 15px; max-width: 90%; left: 15px;">
+                        <br>
+                        <input type="number" min="0" max="20">paletas
+                        <input type="number" min="0" max="20">dulces
+                        <input type="number" min="0" max="20">chocolates
+                        <input type="number" min="0" max="20">helados
+                        <input type="number" min="0" max="20">caramelos
+                        <input type="number" min="0" max="20">pastelillos
+                        <input type="number" min="0" max="20">Cristales
+                        <input type="number" min="0" max="20">Donas
+                        <input type="number" min="0" max="20">animales
+                        <input type="number" min="0" max="20">carritos
+                        <input type="number" min="0" max="20">naves
+                        <input type="number" min="0" max="20">muñecos
+                    </div> 
+                </div>
+
+                <div class="ejercicio" style="background-color: #27AE60">
+                    <h2>¿Tienen la misma cantidad</h2>
+                    <h3>Trabajen en Equipo</h3>
+                    <div>
+                        <br>
+                        <h3 style="text-align: center;">¿Cuál te tocó?</h3>
+                        <h4>1 Jueguen en parejas. Mezclen sus tarjetas y pónganlas con los puntos hacia arriba</h4>
+                        <h4>2 Toma una tarjeta, y sin voltearla anota el número de puntos</h4>
+                        <h4>3 Voltea la tarjeta. Si el número es el que escribiste, te quedas con ella; si no, la regresas debajo de las demás</h4>
+                        <h4>4 El juego termina cuando las tarjetas se acaban</h4>
+                        <br>
+                        <img src="/images/clases/clase2/Clase2 img 3.png" style="width: 90%; height: 300px;">
+                    </div> 
+                </div>
+
+                <br>
+            </form>
+        </div>
+    </body>
+</htm>
---
+++ public/Clase4Profesor.php
@@ -0,0 +1,53 @@
+<?php include 'headerClases.php' ?>
+    <body>
+
+        <div class="container">
+            <h1>Clase 4 Profesor</h1>
+
+            <form>
+
+                <div class="ejercicio" style="background-color: #F39C12">
+                    <h2>La máquina de juntar</h2>
+                    <h3>Trabajen en equipo y metan cosas por los tubos</h3>
+                    <div>
+                        <img src="/images/clases/clase4/Clase 4 img 7.png" style="width: 150px; height: 150px; position: absolute; left: 60%;"><br>
+                        <img src="/images/clases/clase4/Clase 4 img 1.png" style="max-width: 35%; height: 100px;">
+                        <input type="number" min="0" max="5"> <br>
+                        <img src="/images/clases/clase4/Clase 4 img 2.png" style="max-width: 35%; height: 50px;">
+                        <input type="number" min="0" max="5"> <br>
+                        <img src="/images/clases/clase4/Clase 4 img 3.png" style="max-width: 35%; height: 50px;">
+                        <input type="number" min="0" max="5"> <br>
+                        <img src="/images/clases/clase4/Clase 4 img 4.png" style="max-width: 35%; height: 50px;">
+                        <input type="number" min="0" max="5"> <br>
+                        <img src="/images/clases/clase4/Clase 4 img 5.png" style="max-width: 35%; height: 50px;">
+                        <input type="number" min="0" max="5"> <br>
+                        <img src="/images/clases/clase4/Clase 4 img 6.png" style="max-width: 35%; height: 50px;">
+                        <input type="number" min="0" max="5"> <br>
+                    </div> 
+                </div>
+
+                <div class="ejercicio" style="background-color: #27AE60">
+                    <h2>Lanzamiento de moneda</h2>
+                    <h3>Aguilas y soles</h3>
+                    <div>
+                        <br>
+                        <h3 style="text-align: center;">Águilas y soles</h3>
+                        <img src="/images/clases/clase4/Clase 4 img 8.jpg" style="max-width: 25%; max-height: 25%; border-radius: 15px; position: relative; left:250px">
+                        <ol>
+                            <li>Lanza 10 monedas de un peso</li>
+                            <li>¿Cuántas águilas salieron?</li>
+                            <input type="number" min="0" max="10">
+                            <li>¿Cuántos soles salieron?</li>
+                            <input type="number" min="0" max="10">
+                            <li>¿Cuántas águilas y soles salieron en total?</li>
+                            <input type="number" min="0" max="10">
+                        </ol>
+                    </div> 
+                </div>
+
+                <br>
+                
+            </form>
+        </div>
+    </body>
+</htm>
